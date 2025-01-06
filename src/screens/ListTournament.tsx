ae444c45abca4ebf5053677f8fdbf710d38bb784
Luis Roman <lromanp@utp.edu.pe>
2025-01-05 19:10:38 -0500

    changes and create new branch development

--- src/screens/ListTournament.tsx
+++ src/screens/ListTournament.tsx
@@ -2,7 +2,7 @@ import { useTournamentStore } from "../store/globalStore";
 
 export const ListTournament = () => {
   const { submittedForms }: any = useTournamentStore();
-  console.log("sub", submittedForms);
+  console.log("submittedForms", submittedForms);
   return (
     <div className="relative overflow-x-auto bg-zinc-900 h-full w-full">
       <table className="w-full text-sm text-left rtl:text-right text-white dark:text-gray-400">
@@ -23,30 +23,23 @@ export const ListTournament = () => {
           </tr>
         </thead>
         <tbody>
-          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 dark:text-gray-100">
-            <th scope="row" class="px-6 py-4">
-              1
-            </th>
-            <td class="px-6 py-4">Jose Maria</td>
-            <td class="px-6 py-4">Gian franco Pariona</td>
-            <td class="px-6 py-4"></td>
-          </tr>
-          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 dark:text-gray-100">
-            <th scope="row" class="px-6 py-4">
-              2
-            </th>
-            <td class="px-6 py-4">Andre Pando</td>
-            <td class="px-6 py-4">Carlos Rodriguez</td>
-            <td class="px-6 py-4"></td>
-          </tr>
-          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 dark:text-gray-100">
-            <th scope="row" class="px-6 py-4">
-              3
-            </th>
-            <td class="px-6 py-4">Santiago Sturmo</td>
-            <td class="px-6 py-4">Luis Roman</td>
-            <td class="px-6 py-4"></td>
-          </tr>
+          {submittedForms.map((form: any, index: any): any => (
+            <tr
+              key={form.konamiId}
+              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 dark:text-gray-100"
+            >
+              <th scope="row" className="px-6 py-4">
+                {index + 1}
+              </th>
+              <td className="px-6 py-4">{form.name}</td>
+              <td className="px-6 py-4">
+                {index + 1 < submittedForms.length
+                  ? submittedForms[index + 1]?.name
+                  : "N/A"}
+              </td>
+              <td className="px-6 py-4"></td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
